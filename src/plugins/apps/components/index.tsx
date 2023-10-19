35106a18b4c1d51b538a36a7d91fb23141a704f2
Moaz <moazmirza13@gmail.com>
2023-10-20 00:46:09 +0500

    load more btn

--- src/plugins/apps/components/index.tsx
+++ src/plugins/apps/components/index.tsx
@@ -16,6 +16,12 @@ export default function AppsPage({ apps, categories }: AppsPluginContent) {
   const [activeCategory, setActiveCategory] = useState(null);
   const [activeSort, setActiveSort] = useState(SORTS.ALPHABETICAL);
   const [activeQuery, setActiveQuery] = useState('');
+  const [displayedApps, setDisplayedApps] = useState(12); // Number of items initially displayed
+
+  const loadMore = () => {
+    // Increase the number of displayed items by 20 when the "Load More" button is clicked
+    setDisplayedApps(displayedApps + 8);
+  };
 
   const sortedApps = apps
     .sort((a, b) => {
@@ -31,7 +37,9 @@ export default function AppsPage({ apps, categories }: AppsPluginContent) {
       } else {
         return true;
       }
-    });
+    })
+    .filter((app) => !activeCategory || app.category === activeCategory[0])
+    .slice(0, displayedApps); // Use the 'displayedApps' variable
 
   const filters = Object.entries(categories);
 
@@ -70,34 +78,6 @@ export default function AppsPage({ apps, categories }: AppsPluginContent) {
       </li>
     );
   };
-
-  const renderDropdownFilter = (filter) => {
-    const [name, entries] = filter;
-    return (
-      <li
-        key={name}
-        className={clsx(
-          'dropdown__link',
-          styles.dropdownFilterItem,
-          Array.isArray(activeCategory) &&
-            activeCategory[0] === name &&
-            'dropdown__link--active'
-        )}
-        onClick={() => setActiveCategory(filter)}
-      >
-        <span>{name}</span>
-        <span className={clsx('badge badge--secondary', styles.filterBadge)}>
-          {entries.length}
-        </span>
-      </li>
-    );
-  };
-
-  const currentFavs = sortedApps.filter(
-    (app) =>
-      app.isFavorite &&
-      (!Array.isArray(activeCategory) || app.category === activeCategory[0])
-  );
 
   return (
     <Layout>
@@ -149,61 +129,7 @@ export default function AppsPage({ apps, categories }: AppsPluginContent) {
               .sort((a, b) => b[1].length - a[1].length)
               .map((cat) => renderPillFilter(cat))}
           </ul>
-          <ul
-            className={clsx(
-              'dropdown dropdown--hoverable',
-              styles.dropdownFilter
-            )}
-          >
-            <button className="button button--electron button--block">
-              {Array.isArray(activeCategory) ? activeCategory[0] : 'All'}
-            </button>
-            <ul className={clsx('dropdown__menu', styles.dropdownMenu)}>
-              <li
-                className={clsx(
-                  'dropdown__link',
-                  styles.dropdownFilterItem,
-                  activeCategory === null && 'dropdown__link--active'
-                )}
-                onClick={() => setActiveCategory(null)}
-              >
-                All
-              </li>
-              {filters
-                .sort((a, b) => b[1].length - a[1].length)
-                .map((cat) => renderDropdownFilter(cat))}
-            </ul>
-          </ul>
         </div>
-        {currentFavs.length > 0 && (
-          <div
-            className={clsx(
-              styles.favsContainer,
-              'container',
-              'shadow--tl',
-              'margin-bottom--xl'
-            )}
-          >
-            <h2 className={styles.sectionHeader}>Favorites ❤️</h2>
-            <div className={clsx(styles.appCardContainer)}>
-              {currentFavs.map((app) => {
-                return (
-                  <AppCard
-                    key={app.slug}
-                    name={app.name}
-                    description={app.description}
-                    category={app.category}
-                    highlightColor={app.faintColorOnWhite}
-                    logo={`https://raw.githubusercontent.com/erickzhao/apps/master/apps/${app.slug}/${app.slug}-icon-128.png`}
-                    isFavorite={true}
-                    website={app.website}
-                    repository={app.repository}
-                  />
-                );
-              })}
-            </div>
-          </div>
-        )}
         <div
           className={clsx(
             styles.appCardContainer,
@@ -212,26 +138,42 @@ export default function AppsPage({ apps, categories }: AppsPluginContent) {
             'margin-bottom--xl'
           )}
         >
-          {sortedApps
-            .filter(
-              (app) => !activeCategory || app.category === activeCategory[0]
-            )
-            .map((app) => {
-              return (
-                <AppCard
-                  key={app.slug}
-                  name={app.name}
-                  description={app.description}
-                  category={app.category}
-                  highlightColor={app.faintColorOnWhite}
-                  logo={`https://raw.githubusercontent.com/erickzhao/apps/master/apps/${app.slug}/${app.slug}-icon-128.png`}
-                  isFavorite={app.isFavorite}
-                  website={app.website}
-                  repository={app.repository}
-                />
-              );
-            })}
+          {sortedApps.map((app) => (
+            <AppCard
+              key={app.slug}
+              name={app.name}
+              description={app.description}
+              category={app.category}
+              highlightColor={app.faintColorOnWhite}
+              logo={`https://raw.githubusercontent.com/erickzhao/apps/master/apps/${app.slug}/${app.slug}-icon-128.png`}
+              isFavorite={app.isFavorite}
+              website={app.website}
+              repository={app.repository}
+            />
+          ))}
         </div>
+
+        {displayedApps < apps.length && (
+          <button
+            style={{
+              backgroundColor: '#00AEFF',
+              color: '#fff',
+              padding: '16px 30px',
+              border: 'none',
+              borderRadius: '5px',
+              cursor: 'pointer',
+              margin: '0 auto',
+              display: 'block',
+              transition: 'background-color 0.3s',
+              fontSize: 'medium',
+            }}
+            onClick={loadMore}
+            onMouseEnter={(e) => (e.target.style.backgroundColor = '#0088CC')}
+            onMouseLeave={(e) => (e.target.style.backgroundColor = '#00AEFF')}
+          >
+            Load More
+          </button>
+        )}
       </main>
     </Layout>
   );
